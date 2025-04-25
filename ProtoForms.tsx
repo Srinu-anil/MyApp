8685d93bcfbf5674365953d16df03b721902da0e
srinivas <srinuanil06@gmail.com>
2025-04-25 09:41:55 +0530

    changes for pdf download

--- ProtoForms.tsx
+++ ProtoForms.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -6,11 +6,28 @@ import {
   StyleSheet,
   Pressable,
   ScrollView,
-  FlatList
+  FlatList,
+  Button,
+  Platform,
+  Alert,
+  Dimensions,
 } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from './redux/store';
-import { setFieldValue, setActiveSection } from './redux/formSlice';
+import {useSelector, useDispatch} from 'react-redux';
+import {RootState} from './redux/store';
+import {setFieldValue, setActiveSection} from './redux/formSlice';
+import {
+  DocumentDirectoryPath,
+  RoamingDirectoryPath,
+} from '@dr.pogodin/react-native-fs';
+import {
+  completeHandlerIOS,
+  downloadFile,
+  readFile,
+  stopDownload,
+} from '@dr.pogodin/react-native-fs';
+import {unlink} from '@dr.pogodin/react-native-fs';
+import {type PlatformOSType} from 'react-native';
+// import Pdf from 'react-native-pdf';
 
 type Field = {
   label: string;
@@ -23,10 +40,97 @@ type Field = {
 const ProtoForms = () => {
   const formState = useSelector((state: RootState) => state).data;
   const dispatch = useDispatch();
+  const [urlSource, setUrlSource] = useState({uri: '', cache: false});
 
   const handleChange = (name: string, value: string) => {
-    console.log("form state ",formState);
-    dispatch(setFieldValue({ section: formState.activeSection, name, value }));
+    console.log('form state ', formState);
+    dispatch(setFieldValue({section: formState.activeSection, name, value}));
+  };
+
+  const savePdf = async (apiUrl, filePath) => {
+    const CONTENT = 'GÖÖÐ\n';
+    interface ErrorStatus {
+      type: 'error';
+      message?: string;
+    }
+    interface SuccessStatus {
+      type: 'success';
+      message?: string;
+    }
+    interface PendingStatus {
+      type: 'pending';
+    }
+    interface NotAvailableStatus {
+      type: 'notAvailable';
+      message: string;
+    }
+    async function tryUnlink(path: string): Promise<void> {
+      try {
+        await unlink(path);
+      } catch {}
+    }
+
+    const Result = {
+      error: (...message: string[]): ErrorStatus => ({
+        type: 'error',
+        message: message.join(' '),
+      }),
+      catch: (error: any): ErrorStatus => ({
+        type: 'error',
+        message: `${error.code}: ${error.message}`,
+      }),
+      success: (...message: string[]): SuccessStatus => ({
+        type: 'success',
+        message: message.join(' '),
+      }),
+      pending: (): PendingStatus => ({type: 'pending'}),
+      notAvailable: (...platforms: PlatformOSType[]): NotAvailableStatus => ({
+        type: 'notAvailable',
+        message: `not available on ${Platform.OS} but [${platforms.join(
+          ', ',
+        )}]`,
+      }),
+    };
+
+    const PATH = (...path: string[]) =>
+      RoamingDirectoryPath + SEPARATOR + path.join(SEPARATOR) + '.pdf';
+    const SEPARATOR = Platform.OS === 'windows' ? '\\' : '/';
+    const url = apiUrl;
+    const path = PATH('downloadFile-1');
+    setUrlSource({uri: path, cache: true});
+    await tryUnlink(path);
+    try {
+      // execute
+      Alert.alert('path ', path);
+      const {jobId, promise} = downloadFile({
+        fromUrl: url,
+        toFile: path,
+      });
+      const res = await promise;
+
+      // test
+      if (typeof jobId !== 'number') {
+        return Result.error(`type ${typeof jobId} !== number`);
+      }
+      if (res.bytesWritten !== 8) {
+        return Result.error(`bytesWritten ${res.bytesWritten} !== 8`);
+      }
+      if (res.statusCode !== 200) {
+        return Result.error(`statusCode ${res.statusCode} !== 200`);
+      }
+
+      const file = await readFile(path);
+      if (file !== CONTENT) return Result.error(`${file} !== ${CONTENT}`);
+      return Result.success();
+    } catch (e) {
+      return Result.catch(e);
+    }
+  };
+
+  const handlepdfDownload = () => {
+    const apiUrl = 'http://localhost:3000/api/generating-pdf';
+    const filePath = `my_document.pdf`;
+    savePdf(apiUrl, filePath);
   };
 
   const renderField = (field: Field) => {
@@ -60,7 +164,7 @@ const ProtoForms = () => {
                 <View
                   style={[
                     styles.radioCircle,
-                    field.value === option && styles.selectedRadio
+                    field.value === option && styles.selectedRadio,
                   ]}
                 />
                 <Text style={styles.radioLabel}>{option}</Text>
@@ -86,17 +190,36 @@ const ProtoForms = () => {
       formState.activeSection.charAt(0).toLowerCase() +
       formState.activeSection.replace(/\s+/g, '').slice(1);
     const section = (formState as any)[sectionKey];
-    if (!section || !section.fields) return (<Text>{formState.activeSection}</Text>);
+    if (!section || !section.fields)
+      return <Text>{formState.activeSection}</Text>;
 
     return (
       <ScrollView contentContainerStyle={styles.formContent}>
         <Text style={styles.heading}>{formState.activeSection}</Text>
         {section.fields.map((field: Field) => (
-          <View key={field.name} style={{ marginBottom: 20 }}>
+          <View key={field.name} style={{marginBottom: 20}}>
             <Text style={styles.label}>{field.label}</Text>
             {renderField(field)}
           </View>
         ))}
+        <Button title="Download PDF" onPress={handlepdfDownload} />
+
+        {/* <Pdf
+          source={urlSource}
+          onLoadComplete={(numberOfPages, filePath) => {
+            console.log(`Number of pages: ${numberOfPages}`);
+          }}
+          onPageChanged={(page, numberOfPages) => {
+            console.log(`Current page: ${page}`);
+          }}
+          onError={error => {
+            console.log(error);
+          }}
+          onPressLink={uri => {
+            console.log(`Link pressed: ${uri}`);
+          }}
+          style={styles.pdf}
+        /> */}
       </ScrollView>
     );
   };
@@ -108,16 +231,20 @@ const ProtoForms = () => {
         <FlatList
           data={formState.sections}
           keyExtractor={item => item}
-          renderItem={({ item }) => (
+          renderItem={({item}) => (
             <Pressable
               onPress={() => dispatch(setActiveSection(item))}
               style={styles.sidebarItem}>
               <Text
-                style={[styles.sidebarText, item === formState.activeSection && styles.activeSidebarText]}
-              >
+                style={[
+                  styles.sidebarText,
+                  item === formState.activeSection && styles.activeSidebarText,
+                ]}>
                 {item}
               </Text>
-              {item === formState.activeSection && <Text style={styles.checkmark}>✓</Text>}
+              {item === formState.activeSection && (
+                <Text style={styles.checkmark}>✓</Text>
+              )}
             </Pressable>
           )}
         />
@@ -130,68 +257,73 @@ const ProtoForms = () => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    height: '100%'
+    height: '100%',
+  },
+  pdf: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
   sidebar: {
     width: 250,
     backgroundColor: '#f2f2f2',
     padding: 16,
     borderRightWidth: 1,
-    borderRightColor: '#ccc'
+    borderRightColor: '#ccc',
   },
   sidebarTitle: {
     fontWeight: 'bold',
     fontSize: 16,
-    marginBottom: 20
+    marginBottom: 20,
   },
   sidebarItem: {
     flexDirection: 'row',
     alignItems: 'center',
-    paddingVertical: 10
+    paddingVertical: 10,
   },
   sidebarText: {
     fontSize: 14,
-    flex: 1
+    flex: 1,
   },
   activeSidebarText: {
     color: '#0078D4',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   checkmark: {
     color: '#0078D4',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   mainContent: {
     flex: 1,
-    padding: 20
+    padding: 20,
   },
   formContent: {
-    paddingBottom: 60
+    paddingBottom: 60,
   },
   heading: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20
+    marginBottom: 20,
   },
   label: {
     marginTop: 15,
-    fontSize: 16
+    fontSize: 16,
   },
   input: {
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 6,
     padding: 10,
-    marginTop: 5
+    marginTop: 5,
   },
   multiLine: {
     height: 100,
-    textAlignVertical: 'top'
+    textAlignVertical: 'top',
   },
   radioContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 10
+    marginTop: 10,
   },
   radioCircle: {
     height: 16,
@@ -199,14 +331,14 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderWidth: 2,
     borderColor: '#777',
-    marginRight: 10
+    marginRight: 10,
   },
   selectedRadio: {
-    backgroundColor: '#0078D4'
+    backgroundColor: '#0078D4',
   },
   radioLabel: {
-    fontSize: 16
-  }
+    fontSize: 16,
+  },
 });
 
 export default ProtoForms;
